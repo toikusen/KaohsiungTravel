fefd234c3c9d9c83ae95e9f3e97cec88056fcb41
toikusen <tuyucheng0407@gmail.com>
2021-08-09 11:27:45 +0800

    [Update]新增下拉選單選取地區內容，修正縮排

--- js/all.js
+++ js/all.js
@@ -11,23 +11,8 @@ xhr.onload = function(){
     var selectArea = document.querySelector('.selectArea');
     var placeName = document.querySelector('.content')
     
-    function updateArea(e){
-        var selectValue =  e.target.value;
-        var strArea = '';
-        for (var i=0; i<dataLeng; i++){
-            if (selectValue == Data.result.records[i].Zone){
-                strArea = `<h2>${Data.result.records[i].Zone}</h2>`
-                console.log(strArea);
-            }
-        }
-        selectArea.innerHTML = strArea;
-    }
-    select.addEventListener('change',updateArea,false);
-
-
-
-    //用來做下拉選單的
-    //陣列必須放在迴圈外面，不然每做一次迴圈就會產生一個新的陣列
+    //生成地區的陣列
+    //areaList陣列必須放在迴圈外面，不然每做一次迴圈就會產生一個新的陣列
     var areaList = [];
     //設一個迴圈把opendata的資料放入areaList
     for (var i=0; i<dataLeng; i++){
@@ -45,44 +30,67 @@ xhr.onload = function(){
         // select.innerHTML = `<option>${area}</optiion>`
     }
 
-
-    //景點資料
-    var nameList = [];
-    var imgList = [];
-    var timeList = [];
-    var addressList = [];
-    var phoneList = [];
-    var strContent = '';
-
-    for (var i=0; i<dataLeng; i++){
-        nameList = (Data.result.records[i].Name);
-        imgList = (Data.result.records[i].Picture1);
-        timeList = (Data.result.records[i].Opentime);
-        addressList = (Data.result.records[i].Add);
-        phoneList = (Data.result.records[i].Tel);
-        ticketList = (Data.result.records[i].Ticketinfo);
-        strContent +=
-         `
-         <div class="attractions">         
-            <ul>
-                <li>
-                    <div class="picture" style="background:url('${imgList}')">
-                        <h3>${nameList}</h3>   
-                    </div>
-                    <div class="info">                          
-                        <p><img src="./picture/icons_clock.png" alt=""> ${timeList}</p>       
-                        <p><img src="./picture/icons_pin.png" alt=""> ${addressList}</p>  
-                        <p><img src="./picture/icons_phone.png" alt=""> ${phoneList}</p>  
-                    </div>
-                    <div class="ticket">
-                        <p><img src="./picture/icons_tag.png" alt=""> ${ticketList}</p> 
-                    </div>
-                </li>
-            </ul>
-        </div>
-        `
+    //下拉選單更改地區名稱
+    function updateArea(e){
+        var selectValue =  e.target.value;
+        var strArea = '';
+        for (var i=0; i<dataLeng; i++){
+            if (selectValue == Data.result.records[i].Zone){
+                strArea = `<h2>${Data.result.records[i].Zone}</h2>`
+                console.log(strArea);
+            }
+        }
+        selectArea.innerHTML = strArea;
     }
-    placeName.innerHTML = strContent;
+    select.addEventListener('change',updateArea,false);
 
+
+
+    //下拉選單更改地區內容
+    function updateContent(e){
+        var selectValue = e.target.value;
+        //景點資料
+        var nameList = [];
+        var imgList = [];
+        var timeList = [];
+        var addressList = [];
+        var phoneList = [];
+        var strContent = '';
+
+        for (var i=0; i<dataLeng; i++){
+            nameList = (Data.result.records[i].Name);
+            imgList = (Data.result.records[i].Picture1);
+            timeList = (Data.result.records[i].Opentime);
+            addressList = (Data.result.records[i].Add);
+            phoneList = (Data.result.records[i].Tel);
+            ticketList = (Data.result.records[i].Ticketinfo);
+            if (selectValue == Data.result.records[i].Zone){
+                strContent +=
+                `
+                <div class="attractions">         
+                    <ul>
+                        <li>
+                            <div class="picture" style="background:url('${imgList}')">
+                                <h3>${nameList}</h3>   
+                            </div>
+                            <div class="info">                          
+                                <p><img src="./picture/icons_clock.png" alt=""> ${timeList}</p>       
+                                <p><img src="./picture/icons_pin.png" alt=""> ${addressList}</p>  
+                                <p><img src="./picture/icons_phone.png" alt=""> ${phoneList}</p>  
+                            </div>
+                            <div class="ticket">
+                                <p><img src="./picture/icons_tag.png" alt=""> ${ticketList}</p> 
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                `
+            }
+
+        }
+        placeName.innerHTML = strContent;
+        }
+
+    select.addEventListener('change',updateContent,false);
 }
 
